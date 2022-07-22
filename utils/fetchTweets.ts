7f437c237b3a93c4ce81b6a8a21b54998e0c4728
Sanish <Sanish.shyam@gmail.com>
2022-07-22 20:52:48 +0530

    bug fix

--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -2,7 +2,11 @@ import { Tweet } from "../typings";
 import fetch from 'isomorphic-unfetch';
 
 export const fetchTweets = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`)
+
+    const procenv = process.env.NEXT_PUBLIC_BASE_URL
+    const url = `${procenv}/api/getTweets`
+
+    const res = await fetch(url)
 
     const data = await res.json();
 
